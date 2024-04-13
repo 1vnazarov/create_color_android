6f3a37ac688ed57042016d40e44728092d053975
1vnazarov <115411574+1vnazarov@users.noreply.github.com>
2024-04-13 17:59:21 +0300

    По тз

--- app/src/main/java/com/example/create_color/nazarovia/MainActivity.kt
+++ app/src/main/java/com/example/create_color/nazarovia/MainActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.create_color.nazarovia.databinding.ActivityMainBinding
 import com.example.create_color.nazarovia.databinding.DialogColorPickerBinding
@@ -13,7 +14,6 @@ import com.example.create_color.nazarovia.databinding.DialogColorPickerBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var dialogBinding: DialogColorPickerBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,7 +28,15 @@ class MainActivity : AppCompatActivity() {
         dialogBinding = DialogColorPickerBinding.inflate(layoutInflater)
         val dialog = AlertDialog.Builder(this).setTitle("Создай цвет!").setView(dialogBinding.root).setOnDismissListener{
             updateColorPreview()
-        }.setPositiveButton("OK") {_, _ -> updateColorPreview()}
+        }.setPositiveButton("OK", null).create()
+            dialog.setOnShowListener {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                    if (!(validate(dialogBinding.red) && validate(dialogBinding.green) && validate(dialogBinding.blue)))
+                        return@setOnClickListener
+                    updateColorPreview()
+                }
+            }
+
         /*dialogBinding.red.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -62,7 +70,23 @@ class MainActivity : AppCompatActivity() {
         dialog.show()
     }
 
+    private fun validate(textbox: EditText): Boolean {
+        val value = textbox.text.toString().toIntOrNull()
+        return if (!(value != null && value in 0..255)) {
+            textbox.setBackgroundColor(Color.RED)
+            false
+        } else {
+            textbox.setBackgroundColor(Color.TRANSPARENT)
+            true
+        }
+    }
     private fun updateColorPreview() {
-        binding.colorPreview.setBackgroundColor(Color.rgb(dialogBinding.red.text.toString().toIntOrNull() ?: 0, dialogBinding.green.text.toString().toIntOrNull() ?: 0, dialogBinding.blue.text.toString().toIntOrNull() ?: 0))
+        binding.colorPreview.setBackgroundColor(
+            Color.rgb(
+                dialogBinding.red.text.toString().toIntOrNull() ?: 0,
+                dialogBinding.green.text.toString().toIntOrNull() ?: 0,
+                dialogBinding.blue.text.toString().toIntOrNull() ?: 0
+            )
+        )
     }
 }
